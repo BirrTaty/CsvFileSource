b2ac7036db843a006b590805b121f9b37e75ed81
Tatiana <birrtaty@gmail.com>
2021-11-09 11:45:19 +0300

    Modified Test

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
+    private final BonusService service = new BonusService();
+
     @ParameterizedTest
     @CsvFileSource(resources = {"/data.csv"},
     delimiter = '|', numLinesToSkip = 2)
     public void shouldCalculate(long amount, boolean registered, long expected) {
-        BonusService service = new BonusService();
         assertEquals(expected, service.calculate(amount, registered));
     }
 }
